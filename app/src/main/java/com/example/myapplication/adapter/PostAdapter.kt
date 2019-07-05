9e955da96e0fe5d13fb47b013b57f25a0792ea13
moseskamira <moses.african@gmail.com>
2019-07-05 12:25:58 +0300

    Create App to fetch To Dos - Fetch All To Dos and display them to user

---
+++ app/src/main/java/com/example/myapplication/adapter/PostAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.myapplication.adapter
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.myapplication.R
+import com.example.myapplication.model.Post
+import kotlinx.android.synthetic.main.item_list_view.view.*
+
+class PostAdapter(val context: Context, private val availablePosts: ArrayList<Post>): RecyclerView.Adapter<PostAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, position: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_list_view, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return availablePosts.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val postId = holder.postId
+        if (postId != null){
+            postId.text = availablePosts[position].id.toString()
+        }
+
+        val title = holder.title
+        title.text = availablePosts[position].title
+        val body = holder.body
+        body.text = availablePosts[position].body
+
+    }
+
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        var postId = itemView.post_id
+        var title = itemView.title_id
+        var body = itemView.body_id
+//        var parentLayout = itemView.list_item
+
+
+    }
+
+}
